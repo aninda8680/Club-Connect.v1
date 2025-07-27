4db0185de255782f80944a3ae0a0c851665e58f5
aninda8680 <anindadebta8680@gmail.com>
2025-07-27 23:27:26 +0530

    updated

--- src/components/Panels/ADMIN/AdminClubDash.tsx
+++ src/components/Panels/ADMIN/AdminClubDash.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { doc, getDoc, getDocs, collection, updateDoc, setDoc } from "firebase/firestore";
+import { doc, getDoc, getDocs, collection} from "firebase/firestore";
 import { db } from "../../../firebase";
 import { useAuth } from "../../../AuthContext";
 import {
